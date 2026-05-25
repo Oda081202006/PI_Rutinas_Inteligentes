c618ef19c7c58469d4b547576448ae2acd3bbffc
Alejandra Simba <lisseth.simba@udla.edu.ec>
2026-05-25 11:37:24 -0500

    Avance 2

--- src/modelo/MateriasTareas.java
+++ src/modelo/MateriasTareas.java
@@ -10,6 +10,7 @@ public class MateriasTareas {
     private String fechaEntrega;
     private String prioridad;
 
+    //Constructores
     public MateriasTareas(String nombreMateria, String nivelDificultad, double calificacionActual,
                           double notaMinimaPersonal, String nombreTarea, String fechaEntrega) {
         this.nombreMateria = nombreMateria;
@@ -19,11 +20,16 @@ public class MateriasTareas {
         this.nombreTarea = nombreTarea;
         this.fechaEntrega = fechaEntrega;
     }
-    public String getNombreMateria() { return nombreMateria; }
-    public void setNombreMateria(String nombreMateria) { this.nombreMateria = nombreMateria; }
 
-    public String getNivelDificultad() { return nivelDificultad; }
-    public void setNivelDificultad(String nivelDificultad) {
+    //Getter and Setter "nombreMateria"
+    public String getNombreMateria(){ return nombreMateria; }
+    public void setNombreMateria(String nombreMateria){ this.nombreMateria = nombreMateria;}
+
+    //Getter and Setter "nivelDificultad"
+    public String getNivelDificultad(){
+        return nivelDificultad;
+    }
+    public void setNivelDificultad(String nivelDificultad){
         if (nivelDificultad.equalsIgnoreCase("alto") ||
                 nivelDificultad.equalsIgnoreCase("medio") ||
                 nivelDificultad.equalsIgnoreCase("bajo")) {
@@ -32,26 +38,38 @@ public class MateriasTareas {
             this.nivelDificultad = "medio";
         }
     }
-    public double getCalificacionActual() { return calificacionActual; }
+
+    //Getter and Setter "calificacionActual"
+    public double getCalificacionActual(){
+        return calificacionActual;
+    }
     public void setCalificacionActual(double calificacionActual) {
         if (calificacionActual >= 0 && calificacionActual <= 10)
             this.calificacionActual = calificacionActual;
     }
 
-    public double getNotaMinimaPersonal() { return notaMinimaPersonal; }
+    //Getter and Setter "notaMinimaPersonal"
+    public double getNotaMinimaPersonal(){
+        return notaMinimaPersonal;
+    }
     public void setNotaMinimaPersonal(double notaMinimaPersonal) {
         if (notaMinimaPersonal > 0 && notaMinimaPersonal <= 10)
             this.notaMinimaPersonal = notaMinimaPersonal;
     }
+
+    //Getter and Setter "horasRecomendadas"
     public double getHorasRecomendadas() { return horasRecomendadas; }
     public void setHorasRecomendadas(double horasRecomendadas) { this.horasRecomendadas = horasRecomendadas; }
 
+    //Getter and Setter "nombreTarea"
     public String getNombreTarea() { return nombreTarea; }
     public void setNombreTarea(String nombreTarea) { this.nombreTarea = nombreTarea; }
 
+    //Getter and Setter "fechaEntrega"
     public String getFechaEntrega() { return fechaEntrega; }
     public void setFechaEntrega(String fechaEntrega) { this.fechaEntrega = fechaEntrega; }
 
+    //Getter and Setter "prioridad"
     public String getPrioridad() { return prioridad; }
     public void setPrioridad(String prioridad) { this.prioridad = prioridad; }
 
